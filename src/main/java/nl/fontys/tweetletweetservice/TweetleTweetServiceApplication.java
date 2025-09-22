d2ff19b6e29726c4b226d3ce25997984ff447eb4
erde123 <108157433+erde123@users.noreply.github.com>
2025-09-22 11:52:32 +0200

    Initial commit

---
+++ src/main/java/nl/fontys/tweetletweetservice/TweetleTweetServiceApplication.java
@@ -0,0 +1,13 @@
+package nl.fontys.tweetletweetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetleTweetServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TweetleTweetServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/fontys/tweetletweetservice/TweetleTweetServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.fontys.tweetletweetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetleTweetServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
