7560fa0b173d943c98ffef78c4e346376053aef4
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-01 21:07:43 +0200

    add Tweet Model

--- src/main/java/nl/fontys/tweetletweetservice/persistence/document/Tweet.java
+++ src/main/java/nl/fontys/tweetletweetservice/persistence/document/Tweet.java
@@ -1,4 +1,68 @@
 package nl.fontys.tweetletweetservice.persistence.document;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "tweets")
 public class Tweet {
+    @Id
+    private String id;
+    private String userId;
+    private String content;
+    private Date createdAt;
+    private int likes;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet{" +
+                "id='" + id + '\'' +
+                ", userId='" + userId + '\'' +
+                ", content='" + content + '\'' +
+                ", createdAt=" + createdAt +
+                ", likes=" + likes +
+                '}';
+    }
 }
