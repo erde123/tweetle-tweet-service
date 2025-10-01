4c094141f8541b70da46d94545a2bf93ba996ff2
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-01 21:07:24 +0200

    add Security Conifg

---
+++ src/main/java/nl/fontys/tweetletweetservice/configuration/SecurityConfig.java
@@ -0,0 +1,31 @@
+package nl.fontys.tweetletweetservice.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .cors(e -> Customizer.withDefaults())
+//                .csrf(e -> Customizer.withDefaults())
+                .csrf(e -> e.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()   // allow everything
+                )
+//                .authorizeHttpRequests(req -> req.requestMatchers(
+//                                "/api/tweets/**")
+//                        .permitAll()
+//                        .anyRequest()
+//                        .authenticated())
+//                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
