287678ff51e8a6c39004eadb322248e59c74bd88
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-01 21:08:17 +0200

    add tweet service

--- src/main/java/nl/fontys/tweetletweetservice/business/service/TweetService.java
+++ src/main/java/nl/fontys/tweetletweetservice/business/service/TweetService.java
@@ -1,4 +1,21 @@
 package nl.fontys.tweetletweetservice.business.service;
 
+import nl.fontys.tweetletweetservice.persistence.document.Tweet;
+import nl.fontys.tweetletweetservice.persistence.repository.TweetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TweetService {
+    @Autowired private TweetRepository tweetRepository;
+
+    public void save(Tweet tweet) { tweetRepository.save(tweet); }
+
+    public List<Tweet> findAll() { return tweetRepository.findAll(); }
+
+    public Tweet findById (String id) { return tweetRepository.findById(id).orElse(null); }
+
+    public void deleteById (String id) { tweetRepository.deleteById(id); }
 }
